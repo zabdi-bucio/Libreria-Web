d551bd7929f8b32a80ed3a476b61fbbace6d7314
Zabdi Bucio <zabdibucio74@gmail.com>
2026-05-22 01:40:10 -0600

    Habilitar conexion SSL para Azure MySQL

--- config/conexion.php
+++ config/conexion.php
@@ -22,6 +22,7 @@ class Conexion {
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
             PDO::ATTR_EMULATE_PREPARES   => false,
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         ];
 
         try {
